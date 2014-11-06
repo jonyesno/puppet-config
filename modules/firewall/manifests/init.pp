6c28e780bf19a7b48d264859ebc3bf7bca7d0a22
Jon Stuart <jon@zomo.co.uk>
2014-11-06 10:36:27 +0700

    firewall - clean up template, use hiera
    setting $apache, $mysql, $servers NYI

--- modules/firewall/manifests/init.pp
+++ modules/firewall/manifests/init.pp
@@ -1,12 +1,15 @@
 class firewall {
-  file { "/etc/sysconfig/iptables":
+  # FIXME: need to define $apache, $mysql and $servers
+  $manage = hiera('ipaddress_manage', '127.0.0.1')
+
+  file { '/etc/sysconfig/iptables':
     owner   => root,
     group   => root,
-    content => template("firewall/iptables.erb"),
-    notify  => Service["iptables"],
+    content => template('firewall/iptables.erb'),
+    notify  => Service['iptables'],
   }
 
-  service { "iptables":
+  service { 'iptables':
     ensure     => running,
     enable     => true,
     hasstatus  => true,
