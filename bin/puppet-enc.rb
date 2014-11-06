c1b0a6e76d9548157e20a40cc29528485fb85cc0
Jon Stuart <jon@zomo.co.uk>
2014-11-06 10:08:15 +0700

    Replace homegrown YAML merger with hiera

---
+++ bin/puppet-enc.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+systems_yml = File.dirname(__FILE__) + '/../systems.yaml'
+raise RuntimeError, "can't find systems config at #{systems_yml}" unless File.exists?(systems_yml)
+
+system = ARGV.shift
+raise RuntimeError, "usage: puppet-enc.rb servername" if system.nil?
+system("logger -t puppet-enc called for #{system}")
+
+systems = YAML::load_file(systems_yml)
+unless systems.has_key?(system)
+  system("logger -t puppet-enc don't know about #{system}")
+  exit 1
+end
+
+output = { 'parameters' => systems[system] }
+puts YAML::dump(output)
---
+++ bin/query-hiera.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'pp'
+
+base = File.dirname(__FILE__) + '/..'
+systems_yml = "#{base}/systems.yaml"
+hiera_yml   = "#{base}/hiera.yaml"
+raise RuntimeError, "cant't find systems config at #{systems_yml}" unless File.exists?(systems_yml)
+
+system = ARGV.shift
+key    = ARGV.shift
+raise RuntimeError, "usage: query-hiera.rb servername key" if system.nil? || key.nil?
+
+systems = YAML::load_file(systems_yml)
+unless systems.has_key?(system)
+  raise RuntimeError, "can't find #{system} in #{systems_yml}"
+  exit 1
+end
+
+args = [ "::fqdn=#{system}" ]
+systems[system].each { |k,v| args << "::#{k}=#{v}" }
+system("hiera -d -c #{hiera_yml} #{key} #{args.join(' ')}")
