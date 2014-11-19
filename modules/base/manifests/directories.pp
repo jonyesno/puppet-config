d38bf9eb600423bc1de27d6625e1d41ae4e0a7ea
Jon Stuart <jon@zomo.co.uk>
2014-11-19 14:49:10 +0700

    base::directories - /root/log, /var/lib/backup

--- modules/base/manifests/directories.pp
+++ modules/base/manifests/directories.pp
@@ -2,12 +2,16 @@ class base::directories {
   file { [
     '/root',
     '/root/bin',
+    '/root/log',
     ]:
     ensure => directory,
     mode   => '0755', # world readable
   }
 
-  file { '/var/core':
+  file { [
+    '/var/core',
+    '/var/lib/backup',
+    ]:
     ensure => directory,
     owner  => root,
     group  => root,
